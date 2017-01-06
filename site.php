fa03bc5bb9651f0fd5fc1710745919b0d6dba477
sampath kumar challa <challasampath1995@gmail.com>
2017-01-06 15:37:49 +0100

    Add files via upload

---
+++ site.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>User registration
+</title>
+<link href="style.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+<h2> User Registrarion </h2>
+<from action="register.php" method="post">
+<input class="nice" name="text" value="">
+</div>
+</body>
